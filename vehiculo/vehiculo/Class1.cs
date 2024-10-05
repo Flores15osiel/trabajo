00619bccb7ba6093c395674d5a37767a6bbcddd0
Flores15osiel <xinedflores@gmail.com>
2024-10-05 16:52:23 -0600

    este es mi trabajo ticher

---
+++ vehiculo/vehiculo/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vehiculo
+{
+    public class Vehiculo
+    {
+        public string Modelo { get; set; }
+        public double PrecioPorDia { get; set; }
+
+        public class Descuento
+        {
+            public static double AplicarDescuento(double precio, double porcentajeDescuento)
+            {
+                return precio - (precio * porcentajeDescuento / 100);
+            }
+        }
+    }
+
+    public static class CalcularReserva
+    {
+        public static double TotalReserva(Vehiculo vehiculo, int dias, double porcentajeDescuento)
+        {
+            double precioFinal = Vehiculo.Descuento.AplicarDescuento(vehiculo.PrecioPorDia, porcentajeDescuento);
+            return precioFinal * dias;
+        }
+    }
+
+    public static class VehiculoExtensions
+    {
+        public static string MostrarInformacion(this Vehiculo vehiculo)
+        {
+            return $"Modelo: {vehiculo.Modelo}, Precio por día: {vehiculo.PrecioPorDia:C}";
+        }
+    }
+
+
+}
---
+++ vehiculo/vehiculo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using vehiculo;
+
+public class Program
+{
+    public static void Main()
+    {
+        Vehiculo miVehiculo = new Vehiculo
+        {
+            Modelo = "Toyota Corolla",
+            PrecioPorDia = 50.0 
+        };
+
+        double porcentajeDescuento = 10.0; 
+        double precioConDescuento = Vehiculo.Descuento.AplicarDescuento(miVehiculo.PrecioPorDia, porcentajeDescuento);
+
+        Console.WriteLine(miVehiculo.MostrarInformacion());
+
+        int diasReserva = 5;
+        double costoTotal = CalcularReserva.TotalReserva(miVehiculo, diasReserva, porcentajeDescuento);
+
+        Console.WriteLine($"Costo total de la reserva por {diasReserva} días: {costoTotal:C}");
+    }
+}
